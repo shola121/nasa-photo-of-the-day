90e98b2f50adb5f2989036a93b6c6ecc11b5ccfa
shola121 <sholagbenga95@gmail.com>
2023-08-16 17:50:23 +0400

    StyledComponent

--- src/Component/NasaPhoto.js
+++ src/Component/NasaPhoto.js
@@ -1,14 +1,34 @@
 import React  from 'react';
+import styled from 'styled-components';
 
 
+const StyledNasaPhoto = styled.div `
+  border: 1px solid grey;
+  border-radius: 10px;
+  padding : 1.5rem;
+
+  h3 {
+    color : ${props => props.$color};
+    font-size: 2.8em;
+    margin: 0 0 1rem 0;
+  }
+
+  p{
+    margin: 0 0 1.5rem 0;
+     first-line {
+      font-size: 1.5em
+    }
+  }
+`
   const NasaPhoto = (props) => {
     return (
-        <div className = "nasa-photo-wrapper" >
+        <StyledNasaPhoto $color = "Purple" className = "nasa-photo-wrapper" >
             <h3>{props.photo.title}</h3>
             <p>{props.photo.date}</p>
             <img src = {props.photo.hdurl} alt= 'nasaPhoto'/>
             <p>{props.photo.explanation}</p>
-        </div>
+        </StyledNasaPhoto>
     )
   }
-  export default NasaPhoto;
+  export default NasaPhoto 
+  
